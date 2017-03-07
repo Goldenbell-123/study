6023d3edbc66b33d0ff01c41163842ad9c0e935f
buie0906 <buie0906@naver.com>
2017-03-07 17:54:48 +0900

    랜덤함수를 이용하여 난수만들기

---
+++ JavaScript/chap1.keyword/battleship2.js
@@ -0,0 +1,34 @@
+// 배가 위치한방
+
+
+
+var guess; // 추측값
+var hits = 0; // 명중 횟수
+var guesses = 0; // 추측한 횟수 저장W
+var isSunk = false; // 격침 상태 가라앉았는지 여부
+
+while(isSunk==false){
+  var random = Math.floor(Math.random()*5);
+  var location1 = random;
+  var location2 = location1+1;
+  var location3 = location2+1;
+  guess=prompt("준비,조준,발사! (0에서 6까지 중 추측한 숫자를 입력하세요):");
+  if(guess< 0|| guess>6 ||guess==""){
+    alert("0 ~ 6까지의 숫자를 입력하세요.");
+
+  }else{
+    guesses++;
+    if(guess==location1|| guess==location2 || guess==location3){
+      hits++;
+        alert(hits+"회 명중!")
+      if(hits==3){
+        isSunk = true;
+        alert("전함 침몰")
+      }
+    }else{
+      alert("도탄되었습니다!!")
+    }
+  }
+}
+var stats = "발사된 미사일 :"+guesses + "\n명중률 :" +(3/guesses)*100+"%";
+alert(stats);
